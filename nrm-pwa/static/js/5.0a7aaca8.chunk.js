c3c3a4828d2e87b5e67decdab87b4339853b07e9
gio93 <jorgerm1993@gmail.com>
2020-04-24 10:55:33 +0200

    capacitor 2.0 and nrm-pwa

---
+++ nrm-pwa/static/js/5.0a7aaca8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpNimblRippleMethod=this.webpackJsonpNimblRippleMethod||[]).push([[5],{469:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(49),o=(e(11),function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.g)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.m)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.0a7aaca8.chunk.js.map
